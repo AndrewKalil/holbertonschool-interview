736df879ec340cbd2fff1d8cb85a1e2e54859390
AndrewKalil <1541@holbertonschool.com>
2020-12-16 18:12:28 -0500

    fixing betty requirements

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts an int node in a sorted linked list
+ * @head: the pointer to the head node of the list
+ * @number: the number to be inserted
+ * Return: the address of the new node or NULL if it fails
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *temp;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number;
+
+	if (!*head)
+	{
+		new->next = NULL;
+		*head = new;
+		return (*head);
+	}
+
+	temp = *head;
+	if (new->n < temp->n)
+	{
+		new->next = *head;
+		*head = new;
+		return (*head);
+	}
+
+	while (temp->next != NULL && (new->n > temp->next->n))
+	{
+		temp = temp->next;
+	}
+	new->next = temp->next;
+	temp->next = new;
+
+	return (*head);
+}
